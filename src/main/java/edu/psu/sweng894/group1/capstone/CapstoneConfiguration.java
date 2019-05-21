84535a087c575dba52b22ea9df4169efad350d6f
Chris Carcel <cvelo@aol.com>
2019-05-20 21:09:05 -0400

    Configure PostgreSQL on Heroku
    Also update Spring Boot version to latest.

---
+++ src/main/java/edu/psu/sweng894/group1/capstone/CapstoneConfiguration.java
@@ -0,0 +1,9 @@
+package edu.psu.sweng894.group1.capstone;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+
+@Configuration
+@EnableJdbcRepositories
+public class CapstoneConfiguration {
+}
